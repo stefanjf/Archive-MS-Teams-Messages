75b1e56369b44dee7f01f1c51289095dd7c491c6
Stefan Fernandez <stefan.fernandez@gmail.com>
2021-05-25 10:24:48 -0400

    init

---
+++ README.md
@@ -0,0 +1,77 @@
+# Archive MS Teams Channel Messages
+
+## Overview
+
+This was written to support CVR Teams migration efforts. However, it should work on any Teams tenant that is accessible from the Microsoft Graph API.
+
+This code uses the Microsoft Graph API to pull and output all messages (and replies) from a channel(s) in a Team. The function ```pullMessagesIntoJSON``` will download all the messages/replies. This can be saved as JSON or parsed with ```parseJSONintoHTML``` and saved as an HTML file for easy reading. 
+
+Here's an example of what the HTML output looks like:
+
+<img src="imgs/example_html_output.png" alt="ex"
+	title="A cute kitten" width="600"  />
+
+### Prerequisites
+- Python 3
+
+__NOTE: For those archiving from (DOD) CVR Teams, the data may include FOUO/CUI. Recommend only running this code from an appropriate computer.__
+
+### Limitations
+
+This script only works for 'Channel' messages. It does not work for regular 'Chat' messages, which may or may not be possible through the API. I did not investigate this.
+
+## Instructions
+
+### Step 0 - Get an User Auth Token
+
+a) Navigate to https://developer.microsoft.com/en-us/graph/graph-explorer  
+b) Sign in with your CVR Teams account  
+c) Run any query  
+d) Click the 'Access Token' tab and copy the token for later use.   
+
+![token](imgs/token.png)
+
+### Step 1 - Set up
+- Git clone this repository or download ```archive_teams_messages.py``` to your working directory. 
+- Open up a python 3 interactive session in the same working directory.
+  
+### Step 2 - List Groups and Channels
+- In the python interactive session run the following to import the module:
+```
+from archive_teams_messages import *
+```
+
+Set your user auth token that you got from Step 0
+```
+setToken('super-long-auth-token-here')
+```
+
+Then run ```listMyTeams()``` to show Teams and Channels that you belong to. Example output below:
+```
+054cf813-27a2-4c24-98ae-03fad4c495ee   AF/A9A
+        19:17492e041fe04a5489468f189a7bf98b@thread.skype   General
+3055aa2d-b68d-4a8d-861c-0965de9ad15d   AF Analytic Community
+        19:9dccd09179a7478e9a99cd527eec3326@thread.skype   General
+        19:95d88ddbdf8949f4b7a3609db4cb5f4f@thread.skype   Crowdsourcing
+        19:d896e259881847068b1e42532c21f395@thread.skype   AFAC (15A and 1515) Meetings
+        19:26963956733b48398e8434fde7045ec5@thread.skype   Announcements
+```
+### Step 3 (Option A) - Download all messages from __all channels__
+Run ```pullAllChannelMessagesInGroup("groupID")``` where ```groupID``` is the id from the previous step. 
+
+Example for "AF Analytic Community" group:
+```
+pullAllChannelMessagesInGroup('3055aa2d-b68d-4a8d-861c-0965de9ad15d')
+```
+
+This will pull messages from all channels in that group and output ```.json``` and ```.html``` files for each channel in the current working directory.
+
+### Step 3 (Option B) - Download all messages from a __single channel__
+Run ```pullSingleChannelMessagesInGroup("groupID", "channelID")``` where ```groupID``` and ```channelID``` are the relevant ids from the previous step. 
+
+Example for "General" channel of the "AF Analytic Community" group:
+```
+pullSingleChannelMessagesInGroup('3055aa2d-b68d-4a8d-861c-0965de9ad15d', '19:9dccd09179a7478e9a99cd527eec3326@thread.skype')
+```
+
+This will pull messages from that channel and output ```.json``` and ```.html``` files in the current working directory.
---
+++ archive_teams_messages.py
@@ -0,0 +1,162 @@
+
+import requests, json, datetime, re, dateutil.parser, sys
+from yaspin import yaspin
+
+this = sys.modules[__name__]
+
+this.token = ''
+
+def setToken(mytoken):
+    this.token = mytoken
+
+def listMyTeams():
+    response = requests.get('https://graph.microsoft.com/beta/me/joinedTeams', headers={'Authorization': this.token})
+    if (response.status_code == 200):
+        teams = json.loads(response.content)
+        for team in teams['value']:
+            print(team['id'] + '   ' + team['displayName'])
+            responseChannels = requests.get('https://graph.microsoft.com/beta/teams/' + team['id'] + '/channels', headers={'Authorization': this.token})
+            if (responseChannels.status_code == 200):
+                channels = json.loads(responseChannels.content)
+                for channel in channels['value']:
+                    print('\t' + channel['id'] + '   ' + channel['displayName'])
+    else:
+        print('Error: ' + str(response))
+        
+
+def pullAllChannelMessagesInGroup(groupID):
+    response = requests.get('https://graph.microsoft.com/beta/teams/' + groupID + '/channels', headers={'Authorization': this.token})
+    if (response.status_code == 200):
+        channels = json.loads(response.content)
+        for channel in channels['value']:
+            print(channel['id'] + '   ' + channel['displayName'])
+
+            # Get JSON and save to file
+            chat = pullMessagesIntoJSON(groupID, channel['id'])
+            f = open(channel['displayName'] + "_output.json", "w")
+            f.write(json.dumps(chat))
+            f.close()
+            print('Saved raw JSON to ' + str(channel['displayName']) + "_output.json")
+
+            # Parse into HTML and save to file
+            chatHTML = parseJSONintoHTML(chat)
+            f = open(channel['displayName'] + "_output.html", "w")
+            f.write(json.dumps(chatHTML))
+            f.close()
+            print('Saved HTML to ' + str(channel['displayName']) + "_output.html")
+
+        print('All done with ' + str(channel['displayName']))
+    else:
+        print('Error: ' + str(response))
+  
+def pullSingleChannelMessagesInGroup(groupID, channelID):
+        print('Collecting messages from ' + channelID)
+
+        # Get JSON and save to file
+        chat = pullMessagesIntoJSON(groupID, channelID)
+        f = open(channelID + "_output.json", "w")
+        f.write(json.dumps(chat))
+        f.close()
+        print('Saved raw JSON to ' + str(channelID) + "_output.json")
+
+        # Parse into HTML and save to file
+        chatHTML = parseJSONintoHTML(chat)
+        f = open(channelID + "_output.html", "w")
+        f.write(json.dumps(chatHTML))
+        f.close()
+        print('Saved HTML to ' + str(channelID) + "_output.html")
+
+
+def pullfromAPI(url):
+    # print(url)
+    response = requests.get(url, headers={'Authorization': this.token})
+    messages = json.loads(response.content)
+    # print("Pulled " +  " items.")
+    return messages
+
+@yaspin(text="Downloading messages...")
+def pullMessagesIntoJSON(_groupID, _channelID):
+    # Gather all messages
+    allMessagesRaw = []
+    linkToNextBatch = ""
+
+    # Get list of channels
+    messages = pullfromAPI('https://graph.microsoft.com/beta/teams/' + _groupID + '/channels/' + _channelID + '/messages?$top=100')
+    for item in messages["value"]: allMessagesRaw.append(item)
+
+    # If there's another batch
+    if "@odata.nextLink" in messages.keys():
+        linkToNextBatch = messages["@odata.nextLink"]
+        # print('Another batch available')
+
+        while True:
+            messages = pullfromAPI(linkToNextBatch)
+            for item in messages["value"]: allMessagesRaw.append(item)
+
+            if "@odata.nextLink" in messages.keys():
+                linkToNextBatch = messages["@odata.nextLink"]
+                # print('Another batch available')
+            else:
+                break
+
+    # print('Done with pulling messages! Now adding in replies...')
+
+    # For each message, pull replies and add to dict
+    for msg in allMessagesRaw: 
+        replies = pullfromAPI('https://graph.microsoft.com/beta/teams/' + _groupID + '/channels/' + _channelID + '/messages/' + msg["id"] + '/replies')
+        msg["replies"] = []
+
+        if replies['@odata.count'] > 0:
+            for reply in replies["value"]: msg["replies"].append(reply)
+
+            # Check if more
+            if "@odata.nextLink" in replies.keys():
+                while True:
+                    linkToNextBatch = replies["@odata.nextLink"]
+                    # print('Another batch available')
+                    replies = pullfromAPI(linkToNextBatch)
+                    for reply in replies["value"]: msg["replies"].append(reply)
+
+                    # If more replies, repeat, otherwise break
+                    if "@odata.nextLink" in replies.keys():
+                        linkToNextBatch = replies["@odata.nextLink"]
+                        # print('Another batch available')
+                    else:
+                        break
+    print('Collected ' + str(len(allMessagesRaw)) + ' messages!')
+    return allMessagesRaw
+
+def parseJSONintoHTML(jsonChatMessages):
+    finalHTMLOutput = ""
+
+    jsonChatMessages.sort(key=lambda x: dateutil.parser.isoparse(x['createdDateTime']))
+
+    for msg in jsonChatMessages: 
+        b = msg['body']['content']
+        if b is not None:
+            b = re.sub('\n+', '', b)
+            b = re.sub('\t+', '', b)
+
+            finalHTMLOutput += '<hr><hr><h3>' + msg['from']['user']['displayName'] + ':</h3><h5>Created: ' + msg["createdDateTime"] + '</h5>' + b + '<blockquote>'
+
+        msg['replies'].sort(key=lambda x: dateutil.parser.isoparse(x['createdDateTime']))
+        for reply in msg['replies']:
+            try:
+                user = str(reply['from']['user']['displayName'])
+            except TypeError:
+                user = 'unknown'
+                pass
+
+            try:
+                replyContent = reply['body']['content']
+                replyContent = re.sub('\n+', '', replyContent)
+                replyContent = re.sub('\t+', '', replyContent)
+            except TypeError:
+                replyContent = 'unknown'
+                pass
+
+            finalHTMLOutput += '<h3>Reply From: ' + user + '</h3>' + '<h5>Created: ' + reply["createdDateTime"] + '</h5>' + replyContent
+
+        finalHTMLOutput += '</blockquote>'
+    return finalHTMLOutput
+
